01d9121a3873f9e5ba7c52a6019237a7a6e77f18
Prerak Jain <prerakjain1007@gmail.com>
2025-02-13 21:33:02 +0530

    Domain: added chess board class

---
+++ domain/ChessBoard.ts
@@ -0,0 +1,12 @@
+import { getNumericPosition } from "../utils/chessMovesUtils";
+import { IChessBoard } from "./types/IChessBoard";
+
+export class ChessBoard implements IChessBoard {
+
+  constructor() { }
+
+  isValidPosition(position: string): boolean {
+    const [row, col] = getNumericPosition(position);
+    return row >= 0 && row < 8 && col >= 0 && col < 8;
+  }
+}
---
+++ domain/types/IChessBoard.ts
@@ -0,0 +1,3 @@
+export interface IChessBoard {
+  isValidPosition(position: string): boolean;
+}
