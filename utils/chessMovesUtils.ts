ee48b72a6554cb881f4e4cbd8f887cd033943187
Prerak Jain <prerakjain1007@gmail.com>
2025-02-13 19:12:50 +0530

    added chess moves utils

---
+++ utils/chessMovesUtils.ts
@@ -0,0 +1,13 @@
+export function getNumericPosition(position: string): [number, number] {
+  const columnIndex = position[0].toUpperCase().charCodeAt(0) - "A".charCodeAt(0);
+  const rowIndex = parseInt(position[1], 10) - 1;
+
+  return [rowIndex, columnIndex];
+}
+
+export function getChessNotation(rowIndex: number, columnIndex: number): string {
+  const columnLetter = String.fromCharCode("A".charCodeAt(0) + columnIndex);
+  const rowNumber = (rowIndex + 1).toString();
+
+  return `${columnLetter}${rowNumber}`;
+}
